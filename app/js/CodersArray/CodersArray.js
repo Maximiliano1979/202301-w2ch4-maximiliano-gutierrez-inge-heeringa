0f2d58f8d3d02c2d91c34fdc58b1734b8f7eed80
Inge Heeringa <inge.heeringa@gmail.com>
2023-01-20 00:57:47 +0100

    Create some method and tests

--- app/js/CodersArray/CodersArray.js
+++ app/js/CodersArray/CodersArray.js
@@ -12,6 +12,16 @@ class CodersArray {
     this[this.length] = element;
     this.length++;
   }
+
+  some(callback) {
+    for (let i = 0; i < this.length; i++) {
+      if (callback(this[i])) {
+        return true;
+      }
+    }
+
+    return false;
+  }
 }
 
 export default CodersArray;
